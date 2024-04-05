2e99bef6e4d4010c21d31f00dfda2bdda4aba1d0
Ubuntu <ubuntu@ip-172-31-30-84.eu-west-2.compute.internal>
2024-04-05 13:52:17 +0000

    DOC: typing and doc utils file

--- therapanacea_project/utils/files.py
+++ therapanacea_project/utils/files.py
@@ -1,20 +1,25 @@
 import os
 
-from omegaconf import OmegaConf
 import yaml
+from omegaconf import OmegaConf
 
 
-def make_exists(file_path):
+def make_exists(file_path: str) -> None:
+    """Create the directory if it does not exist."""
     if not os.path.exists(file_path):
         os.makedirs(file_path)
 
 
-def load_yaml(path: str):
+def load_yaml(path: str) -> OmegaConf:
+    """Load a YAML file and return an OmegaConf configuration."""
     yaml_file = OmegaConf.load(path)
     return yaml_file
 
 
-def load_yaml_(path: str):
+def load_yaml_full_loader(path: str) -> OmegaConf:
+    """Load a YAML file and return an OmegaConf configuration.
+    Use this function if YAML file contains special characters.
+    """
     yaml_file = open(path, "r")
     config = yaml.load(yaml_file, Loader=yaml.FullLoader)
     config = OmegaConf.create(config)
